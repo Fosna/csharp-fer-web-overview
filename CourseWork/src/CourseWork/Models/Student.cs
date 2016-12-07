97ef266b1e84e2f78275ca2978eee100654aa389
Fosna <vedran.marsic.ka@gmail.com>
2016-12-07 05:20:06 +0100

    Pretty labels on student details.

--- CourseWork/src/CourseWork/Models/Student.cs
+++ CourseWork/src/CourseWork/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,13 @@ namespace CourseWork.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Student Code")]
         public string StudentCode { get; set; }
 
     }
